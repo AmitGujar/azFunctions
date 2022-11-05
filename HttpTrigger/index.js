cc8579f0e227af5a1836144e43e92868a6ae21e9
AmitGujar <amithero3342@gmail.com>
2022-11-06 02:11:31 +0530

    file changed

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -2,9 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = "Amit"
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const responseMessage = "Hello, " + name + ". This HTTP triggered function executed successfully."
 
     context.res = {
         // status: 200, /* Defaults to 200 */
